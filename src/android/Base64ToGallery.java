06840c27731dabe50d770aaecdcfb77ad5dccc2b
happyi <loveyt_happy@aliyun.com>
2024-06-19 16:22:53 +0800

    解决android10图片无法保存的问题

--- src/android/Base64ToGallery.java
+++ src/android/Base64ToGallery.java
@@ -1,4 +1,6 @@
-package it.nexxa.base64ToGallery;
+package it.nexxa.Base64ToGallery;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -10,7 +12,9 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -21,7 +25,7 @@ import android.util.Log;
 
 /**
  * Base64ToGallery.java
- *
+ * <p>
  * Android implementation of the Base64ToGallery for iOS.
  * Inspirated by Joseph's "Save HTML5 Canvas Image to Gallery" plugin
  * http://jbkflex.wordpress.com/2013/06/19/save-html5-canvas-image-to-gallery-phonegap-android-plugin/
@@ -32,13 +36,20 @@ public class Base64ToGallery extends CordovaPlugin {
 
   // Consts
   public static final String EMPTY_STR = "";
-
+  public static final String ANDROID_WRITE_EXTERNAL_STORAGE = "android.permission.WRITE_EXTERNAL_STORAGE";
+  public static final int REQUEST_CODE_ENABLE_PERMISSION = 55433;
   @Override
   public boolean execute(String action, JSONArray args,
-      CallbackContext callbackContext) throws JSONException {
+                         CallbackContext callbackContext) throws JSONException {
 
-    String base64               = args.optString(0);
-    String filePrefix           = args.optString(1);
+    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+      if (!cordova.hasPermission(ANDROID_WRITE_EXTERNAL_STORAGE)) {
+        cordova.requestPermission(this, REQUEST_CODE_ENABLE_PERMISSION, ANDROID_WRITE_EXTERNAL_STORAGE);
+      }
+    }
+
+    String base64 = args.optString(0);
+    String filePrefix = args.optString(1);
     boolean mediaScannerEnabled = args.optBoolean(2);
 
     // isEmpty() requires API level 9
@@ -48,7 +59,7 @@ public class Base64ToGallery extends CordovaPlugin {
 
     // Create the bitmap from the base64 string
     byte[] decodedString = Base64.decode(base64, Base64.DEFAULT);
-    Bitmap bmp           = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+    Bitmap bmp = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
     if (bmp == null) {
       callbackContext.error("The image could not be decoded");
@@ -75,19 +86,15 @@ public class Base64ToGallery extends CordovaPlugin {
 
   private File savePhoto(Bitmap bmp, String prefix) {
     File retVal = null;
-
     try {
-      String deviceVersion = Build.VERSION.RELEASE;
-      Calendar c           = Calendar.getInstance();
-      String date          = EMPTY_STR
-                              + c.get(Calendar.YEAR)
-                              + c.get(Calendar.MONTH)
-                              + c.get(Calendar.DAY_OF_MONTH)
-                              + c.get(Calendar.HOUR_OF_DAY)
-                              + c.get(Calendar.MINUTE)
-                              + c.get(Calendar.SECOND);
-
-      int check = deviceVersion.compareTo("2.3.3");
+      Calendar c = Calendar.getInstance();
+      String date = EMPTY_STR
+              + c.get(Calendar.YEAR)
+              + c.get(Calendar.MONTH)
+              + c.get(Calendar.DAY_OF_MONTH)
+              + c.get(Calendar.HOUR_OF_DAY)
+              + c.get(Calendar.MINUTE)
+              + c.get(Calendar.SECOND);
 
       File folder;
 
@@ -96,15 +103,16 @@ public class Base64ToGallery extends CordovaPlugin {
        * Environment.DIRECTORY_PICTURES ); //this throws error in Android
        * 2.2
        */
-      if (check >= 1) {
+      if (Build.VERSION.SDK_INT >= 30) {
+        folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+      } else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD_MR1) {
         folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-
-        if (!folder.exists()) {
-          folder.mkdirs();
-        }
-
-      } else {
+      }else {
         folder = Environment.getExternalStorageDirectory();
+      }
+
+      if (!folder.exists()) {
+        folder.mkdirs();
       }
 
       File imageFile = new File(folder, prefix + date + ".png");
@@ -117,6 +125,7 @@ public class Base64ToGallery extends CordovaPlugin {
       retVal = imageFile;
 
     } catch (Exception e) {
+      e.printStackTrace();
       Log.e("Base64ToGallery", "An exception occured while saving image: " + e.toString());
     }
 
@@ -129,7 +138,7 @@ public class Base64ToGallery extends CordovaPlugin {
    */
   private void scanPhoto(File imageFile) {
     Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-    Uri contentUri         = Uri.fromFile(imageFile);
+    Uri contentUri = Uri.fromFile(imageFile);
 
     mediaScanIntent.setData(contentUri);
 
